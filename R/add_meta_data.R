8b4fbe4169288f1e8b3b765d4aff2da75307afa6
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2022-05-24 02:08:23 -0300

    feat: write df to format

---
+++ R/add_meta_data.R
@@ -0,0 +1,23 @@
+add_met_data <- function(df, pol, date_format, date_col, id, from_here=TRUE,
+                         csv_path=getwd()){
+  # Tranforming date to require format
+  df_date <- transform_date_format(df, date_format, date_col)
+  df <- df[, c("date", pol)]
+
+  # Creating file_name
+  start_year <- regmatches(df$date[1], regexpr("[0-9]{4}", df$date[1]))
+  end_year <- regmatches(df$date[nrow(df)], regexpr("[0-9]{4}", df$date[nrow(df)]))
+  file_name <- paste0(toupper(pol), "_", id, "_",
+                      start_year, "_", end_year, ".csv")
+  file_name <- paste0(csv_path, "/", file_name)
+
+  # Adding attributes
+  add_header_line(file_name, "Station_id", id, append = FALSE)
+  if (from_here){
+    add_header_line(file_name, "Station_timezone", Sys.timezone())
+    add_header_line(file_name, "Timeshift_from_UTC", as.POSIXlt(Sys.time())$zone)
+  }
+  cat(paste0("\"Time; value\"", toupper(pol), "\n"), file = file_name)
+  write.table(df, file_name, sep=";", row.names = FALSE, col.names = FALSE,
+              append = TRUE)
+}
