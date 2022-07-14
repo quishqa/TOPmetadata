5e59007bbfa4b6667e40c39cfdedf1d2afc8ef28
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2022-07-14 13:32:42 -0300

    fix: correct TOAR date format

--- R/transform_date_format.R
+++ R/transform_date_format.R
@@ -11,10 +11,10 @@ transform_date_format <- function(df, date_format="%Y-%m-%d %H:%M",
                                   date_col = "date"){
   if (class(df[[date_col]])[1] == "character"){
     df$date <- as.POSIXct(strptime(df[[date_col]], format=date_format))
-    df$date <- format(df$date, format="%d/%m/%Y %H:%M")
+    df$date <- format(df$date, format="%Y-%m-d% %H:%M")
   }
   if (class(df[[date_col]])[1] == "POSIXct"){
-    df$date <- format(df[[date_col]], format = "%d/%m/%Y %H:%M")
+    df$date <- format(df[[date_col]], format = "%Y-%m-%d %H:%M")
   }
   return(df)
 }
