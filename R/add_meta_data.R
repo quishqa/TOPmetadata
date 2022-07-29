5f0e0fa037d65be088080e4dc061e03d57b374cd
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2022-07-28 23:24:55 -0300

    feat: if empty do nothing

--- R/add_meta_data.R
+++ R/add_meta_data.R
@@ -26,28 +26,32 @@
 #' add_met_data(pin_o3, "o3", "%Y-%m-%d %H:%M", "date", pin_meta,
 #'             csv_path = "~/")
 #' }
-add_met_data <- function(df, pol, aqs_meta, date_format = "%Y-%m-%d %H:%M",
+add_meta_data <- function(df, pol, aqs_meta, date_format = "%Y-%m-%d %H:%M",
                          date_col = "date", csv_path=getwd()){
   # Tranforming date to require format
   df_date <- transform_date_format(df, date_format, date_col)
   df <- df_date[, c("date", pol)]
 
-  # Creating file_name
-  start_year <- regmatches(df$date[1], regexpr("[0-9]{4}", df$date[1]))
-  end_year <- regmatches(df$date[nrow(df)],
-                         regexpr("[0-9]{4}", df$date[nrow(df)]))
-  file_name <- paste0(toupper(pol), "_", aqs_meta["Station_id"], "_",
-                      start_year, "-", end_year, ".dat")
-  file_name <- paste0(csv_path, "/", file_name)
+  if (nrow(df) == 0){
+    message("No data in this station")
+  } else {
+    # Creating file_name
+    start_year <- regmatches(df$date[1], regexpr("[0-9]{4}", df$date[1]))
+    end_year <- regmatches(df$date[nrow(df)],
+                           regexpr("[0-9]{4}", df$date[nrow(df)]))
+    file_name <- paste0(toupper(pol), "_", aqs_meta["Station_id"], "_",
+                        start_year, "-", end_year, ".dat")
+    file_name <- paste0(csv_path, "/", file_name)
 
-  # Adding attributes
-  add_header_line(file_name, "Station_id", aqs_meta["Station_id"])
-  for (key in names(aqs_meta)){
-    add_header_line(file_name, key, aqs_meta[key])
+    # Adding attributes
+    add_header_line(file_name, "Station_id", aqs_meta["Station_id"])
+    for (key in names(aqs_meta)){
+      add_header_line(file_name, key, aqs_meta[key])
+    }
+    cat(paste0("\"Time; value\";", "\n"), file = file_name,
+        append = TRUE)
+    utils::write.table(df, file_name, sep=";", row.names = FALSE,
+                       col.names = FALSE, append = TRUE, quote = FALSE)
+    message(paste(file_name), " was created")
   }
-  cat(paste0("\"Time; value\";", toupper(pol), "\n"), file = file_name,
-      append = TRUE)
-  utils::write.table(df, file_name, sep=";", row.names = FALSE,
-                     col.names = FALSE, append = TRUE, quote = FALSE)
-  message(paste(file_name), " was created")
 }
